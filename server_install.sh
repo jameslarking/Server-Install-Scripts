fda2a6181b98ef1c9e6bdac10e7650bd5dc1d783
James Larking <jim@citadelsecure.com>
2011-01-08 07:45:55 +0000

    addedScripts

---
+++ server_install.sh
@@ -0,0 +1,22 @@
+yum update
+yum install httpd
+yum install mysql-server
+yum install php
+yum install php-devel
+chkconfig --level 3 mysqld on
+chkconfig --level 3 httpd on
+
+if [ ! -d "/etc/httpd/sites-enabled" ]; then
+	mkdir /etc/httpd/sites-enabled
+	if [ ! -d "/var/sites" ]; then
+		mkdir /var/sites
+		git clone git://github.com/jameslarking/Server-Default-Holding-Site.git /var/sites/1-default
+	fi	
+fi
+cp 1-default.conf /etc/httpd/sites-enabled/
+cd /var/sites/1-default
+git pull origin master
+
+/etc/init.d/httpd restart
+
+#cat /dev/urandom | base64 | head -c8
